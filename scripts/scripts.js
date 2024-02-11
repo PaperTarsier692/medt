6691991612be00a989a9ea4edd961cb1aff9886d
PaperTarsier692 <118065958+PaperTarsier692@users.noreply.github.com>
2024-02-11 17:52:37 +0100

    Formating

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,7 @@
 function toggle_dark_mode() {
   var element = document.body;
   element.classList.toggle("dark-mode");
-  var button = document.getElementById('darkModeButton')
-  button.classList.toggle('dark-mode');
-  button.classList.toggle('light-mode');
+  var button = document.getElementById("darkModeButton");
+  button.classList.toggle("dark-mode");
+  button.classList.toggle("light-mode");
 }
